857c2e0619113887b1ee13bcb518d0cf3bc950e3
Mysterken <Mysterkenzo@gmail.com>
2024-12-18 11:57:00 +0100

    add currency_historical route

--- money-api/src/Controller/CurrencyController.php
+++ money-api/src/Controller/CurrencyController.php
@@ -12,21 +12,6 @@ class CurrencyController extends AbstractController
 {
     const API_URL = 'https://api.freecurrencyapi.com';
 
-    /**
-     * @throws GuzzleException
-     */
-    #[Route('/api/currency_list', name: 'app_currency_list')]
-    public function list(): JsonResponse
-    {
-        $client = new Client();
-
-        $res = $client->request('GET', self::API_URL . '/v1/currencies', [
-            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
-        ]);
-
-        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
-    }
-
     #[Route('/api/currency_status', name: 'app_currency_status')]
     public function status(): JsonResponse
     {
@@ -42,12 +27,39 @@ class CurrencyController extends AbstractController
     /**
      * @throws GuzzleException
      */
+    #[Route('/api/currency_list', name: 'app_currency_list')]
+    public function list(): JsonResponse
+    {
+        $client = new Client();
+
+        $res = $client->request('GET', self::API_URL . '/v1/currencies', [
+            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
+        ]);
+
+        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
+    }
+
+    /**
+     * @throws GuzzleException
+     */
     #[Route('/api/currency_latest', name: 'app_currency_latest')]
     public function latest(): JsonResponse
     {
         $client = new Client();
 
         $res = $client->request('GET', self::API_URL . '/v1/latest', [
+            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
+        ]);
+
+        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
+    }
+
+    #[Route('/api/currency_historical', name: 'app_currency_historical')]
+    public function historical(): JsonResponse
+    {
+        $client = new Client();
+
+        $res = $client->request('GET', self::API_URL . '/v1/historical', [
             'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
         ]);
 
