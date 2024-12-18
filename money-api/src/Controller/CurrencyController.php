f2529e1a1274f8fa677444981b9815d9ff48ac3f
Mysterken <Mysterkenzo@gmail.com>
2024-12-18 11:58:15 +0100

    refactor controller

--- money-api/src/Controller/CurrencyController.php
+++ money-api/src/Controller/CurrencyController.php
@@ -6,63 +6,51 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CurrencyController extends AbstractController
 {
-    const API_URL = 'https://api.freecurrencyapi.com';
+    private const API_URL = 'https://api.freecurrencyapi.com';
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    private function fetchData(string $endpoint): array
+    {
+        $response = $this->client->request('GET', self::API_URL . $endpoint, [
+            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
 
     #[Route('/api/currency_status', name: 'app_currency_status')]
     public function status(): JsonResponse
     {
-        $client = new Client();
-
-        $res = $client->request('GET', self::API_URL . '/v1/status', [
-            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
-        ]);
-
-        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
+        return new JsonResponse($this->fetchData('/v1/status'));
     }
 
-    /**
-     * @throws GuzzleException
-     */
     #[Route('/api/currency_list', name: 'app_currency_list')]
     public function list(): JsonResponse
     {
-        $client = new Client();
-
-        $res = $client->request('GET', self::API_URL . '/v1/currencies', [
-            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
-        ]);
-
-        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
+        return new JsonResponse($this->fetchData('/v1/currencies'));
     }
 
-    /**
-     * @throws GuzzleException
-     */
     #[Route('/api/currency_latest', name: 'app_currency_latest')]
     public function latest(): JsonResponse
     {
-        $client = new Client();
-
-        $res = $client->request('GET', self::API_URL . '/v1/latest', [
-            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
-        ]);
-
-        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
+        return new JsonResponse($this->fetchData('/v1/latest'));
     }
 
     #[Route('/api/currency_historical', name: 'app_currency_historical')]
     public function historical(): JsonResponse
     {
-        $client = new Client();
-
-        $res = $client->request('GET', self::API_URL . '/v1/historical', [
-            'query' => ['apikey' => $_ENV['CURRENCY_APIKEY']]
-        ]);
-
-        return new JsonResponse(json_decode($res->getBody()->getContents(), true));
+        return new JsonResponse($this->fetchData('/v1/historical'));
     }
 }
