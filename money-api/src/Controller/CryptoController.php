a06947937794959b7b7b6480ab2fbf173a7bc28e
Mysterken <Mysterkenzo@gmail.com>
2024-12-19 15:22:08 +0100

    stock crypto list in coins

--- money-api/src/Controller/CryptoController.php
+++ money-api/src/Controller/CryptoController.php
@@ -2,21 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Coin;
+use App\Repository\CoinRepository;
 use DateMalformedStringException;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CryptoController extends AbstractController
 {
     private const API_URL = 'https://api.coingecko.com/api';
     private Client $client;
 
-    public function __construct()
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly CoinRepository         $coinRepository,
+        private readonly SerializerInterface    $serializer
+    )
     {
         $this->client = new Client();
     }
@@ -43,7 +51,25 @@ class CryptoController extends AbstractController
     #[Route('/api/crypto_list', name: 'app_crypto_list')]
     public function list(): JsonResponse
     {
-        return new JsonResponse($this->fetchData('/v3/coins/list'));
+        ini_set('memory_limit', '256M');
+
+        $DBcoins = $this->coinRepository->findAll();
+
+        if (empty($DBcoins)) {
+            foreach ($this->fetchData('/v3/coins/list') as $coin) {
+                $coinEntity = new Coin();
+                $coinEntity->setId($coin['id']);
+                $coinEntity->setSymbol($coin['symbol']);
+                $coinEntity->setName($coin['name']);
+
+                $this->entityManager->persist($coinEntity);
+                $DBcoins[] = $coinEntity;
+            }
+
+            $this->entityManager->flush();
+        }
+
+        return new JsonResponse($this->serializer->normalize($DBcoins, 'json'));
     }
 
     /**
