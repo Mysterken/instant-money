d414805fc4d49f992e2a8f58faeedaddb4d62653
Mysterken <Mysterkenzo@gmail.com>
2024-12-20 10:10:19 +0100

    add route to display all available one

--- money-api/src/Controller/TestController.php
+++ money-api/src/Controller/TestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,5 +17,26 @@ class TestController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TestController.php',
         ]);
+    }
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    #[Route('/api/test/routes', name: 'app_test_routes')]
+    public function routes(): JsonResponse
+    {
+        $routes = $this->container->get('router')->getRouteCollection();
+
+        $data = [];
+        foreach ($routes as $route) {
+            $data[] = [
+                'path' => $route->getPath(),
+                'name' => $route->getDefault('_controller'),
+                'methods' => $route->getMethods(),
+            ];
+        }
+
+        return $this->json($data);
     }
 }
