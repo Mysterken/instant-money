65ebe8131e3214407a267fc0a1fd042b5b140279
Mysterken <Mysterkenzo@gmail.com>
2024-12-20 12:05:49 +0100

    restore vite default page

--- money-app/src/App.jsx
+++ money-app/src/App.jsx
@@ -1,90 +1,35 @@
-/*import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [rates, setRates] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    // Appel API vers le backend Symfony
-    axios.get("http://localhost:8000/api/rates")
-      .then((response) => {
-        setRates(response.data.rates);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("Erreur API :", err);
-        setError("Erreur lors du chargement des taux de change.");
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>Taux de Change</h1>
-      {loading ? (
-        <p>Chargement...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        <ul>
-          {Object.entries(rates).map(([currency, value]) => (
-            <li key={currency}>
-              {currency} : {value}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
 }
 
-export default App;*/
-
-//API -> JSON
-
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-function App() {
-  const [rates, setRates] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // Charger le fichier JSON local
-    axios.get("/rates.json")
-      .then((response) => {
-        setRates(response.data.rates);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("Erreur lors du chargement du fichier JSON :", err);
-        setError("Erreur lors du chargement des taux.");
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>Taux de Changes</h1>
-      {loading ? (
-        <p>Chargement...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        <ul>
-          {Object.entries(rates).map(([currency, value]) => (
-            <li key={currency}>
-              {currency} : {value}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-}
-
-export default App;
-
+export default App
